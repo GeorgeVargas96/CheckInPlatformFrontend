669d5739aa805124d8caa87969ab16dd40a0f9ba
Andrei Bota <andreibota00@gmail.com>
2021-08-11 09:51:12 +0300

    Small changes

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,3 +1,4 @@
+import { FeatureService } from './../services/feature/feature.service';
 import { Feature } from 'src/app/classes/feature';
 import { Event } from './../classes/event';
 import { Course } from './../classes/course';
@@ -9,7 +10,7 @@ import { ClassroomService } from '../services/classroom/classroom.service';
 
 import {addDays} from 'date-fns';
 import Swal from 'sweetalert2';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Component({
@@ -18,6 +19,15 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
   styleUrls: ['./main-content.component.scss']
 })
 export class MainContentComponent implements OnInit {
+
+  public eventCourseId: number | undefined;
+  public eventCourse: Course | undefined;
+
+  public eventClassroomId: number | undefined;
+  public eventClassroom: Classroom | undefined;
+
+  public eventFeatureId: number | undefined;
+  public eventFeature: Feature | undefined;
 
   courses: Course[]=[
     {
@@ -63,7 +73,6 @@ export class MainContentComponent implements OnInit {
     }
   ];
 
-
   events: Event[] =[{
     id: 1,
     course: this.courses[0],
@@ -91,26 +100,92 @@ export class MainContentComponent implements OnInit {
   constructor(
     private classroomService: ClassroomService,
     private courseService: CourseService,
-    public dialog: MatDialog,
+    private featureService: FeatureService,
     private router: Router) { }
 
   ngOnInit(): void {
+    // this.getEventClassroomId();
+    // this.getEventClassroom();
+
+    // this.getEventCourseId();
+    // this.getEventClassroom();
+
+    // this.getEventFeatureId();
+    // this.getEventFeature();
   }
 
 
+  public goToAddEvent(){
+    this.router.navigateByUrl('add-event');
+  }
 
+  public goToCourses(){
+    this.router.navigateByUrl('courses');
+  }
 
-  hourGrid=[8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+  public getEventCourseId(): void{
+    this.courseService.getEditCourseId().subscribe(
+      courseId => this.eventCourseId = courseId
+    );
+  }
+
+  public getEventCourse(): void{
+    if(this.eventCourseId !== undefined){
+      this.courseService.getCourseById(this.eventCourseId).subscribe(
+        (response: Course) => {
+          console.log(response);
+          this.eventCourse = response;
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      );
+    }
+  }
+
+  public getEventClassroomId(): void{
+    this.classroomService.getEditClassroomId().subscribe(
+      classroomId => this.eventClassroomId = classroomId
+    );
+  }
+
+  public getEventClassroom(): void{
+    if(this.eventClassroomId !== undefined){
+      this.classroomService.getClassroomById(this.eventClassroomId).subscribe(
+        (response: Classroom) => {
+          console.log(response);
+          this.eventClassroom = response;
+        },
+        (error: HttpErrorResponse) =>{
+          alert(error.message);
+        }
+      );
+    }
+  }
+
+  public getEventFeatureId(): void{
+    this.featureService.getEditFeatureId().subscribe(
+      featureId => this.eventFeatureId = featureId
+    );
+  }
+
+  public getEventFeature(): void{
+    if(this.eventFeatureId!== undefined){
+      this.featureService.getFeatueById(this.eventFeatureId).subscribe(
+        (response: Feature) =>{
+          console.log(response);
+          this.eventFeature = response;
+        },
+        (error: HttpErrorResponse) =>{
+          alert(error.message);
+        }
+      )
+    }
+  }
 
   date: Date= new Date();
   firstDayOfWeek= this.date.getDate()-this.date.getDay();
   currentWeek=this.getDaysOfWeek();
-
-
-
-  public getEventCourseName(): void{
-
-  }
 
   getDaysOfWeek(){
     return [
@@ -126,6 +201,8 @@ export class MainContentComponent implements OnInit {
 
    showDetails(event : Event) : void {
      Swal.fire({
+       showCancelButton: true,
+       showConfirmButton: false,
        html:
        '<h1><b>' + event.course.name + '</b></h1>' +
        'Course Name: '  + event.course.name + '<br> <br>'+
